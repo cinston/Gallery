59bf153cd50b5a96b1cc33be03f2b497532b6307
Winston <wagawc@gmail.com>
2019-03-04 17:20:51 +0300

    create error handling to make sure the user inserts a proper date

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
@@ -31,6 +31,15 @@ def convert_dates(dates):
 
 def past_days_photos(request,past_date):
         # Converts data from the string Url
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
     day = convert_dates(date)

23c1d6ba69d3c5a8ea081f8b40bec93447754033
Winston <wagawc@gmail.com>
2019-03-04 16:57:33 +0300

    create URLpattern for the date view

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import urls
+from django.conf.urls import url
 from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^today/$',views.photos_of_day,name='photosToday')
+
 ]

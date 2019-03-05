1165e77c4dad7662e2d69fed7e073bdfc3b73577
Winston <wagawc@gmail.com>
2019-03-05 14:59:13 +0300

    add test cases for our models

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    '''
+    Defines test cases for the Location class behaviours.
+      
+    Args:
+        TestCase: A class that helps create the test cases.
+    '''
+    def setUp(self):
+        '''
+        Method that runs before the test cases.
+        '''
+        self.location = Location(id = 1,name = 'Nairobi')

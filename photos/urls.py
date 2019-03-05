c3a1d6628f8c1ea9873d7b32de85b13d8feb85cc
Winston <wagawc@gmail.com>
2019-03-05 15:28:04 +0300

    add url paths of location,search and index

--- photos/urls.py
+++ photos/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
+urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^location/(\d+)', views.location, name='location'),
     url(r'^results/', views.search, name='search')
-
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

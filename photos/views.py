8c860464eee1dd507cccdc8f87ff637f9fecb979
Winston <wagawc@gmail.com>
2019-03-05 13:14:10 +0300

    add an index, location and search functions to the views

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404
-import datetime as dt
-
+from .models import Image, Location, Category
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
-    return HttpResponse('Welcome to my Gallery')
+def index(request):
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request, 'index.html', {'images': images, 'locations': locations})
 
+def location(request,location):
+    selected_location = Location.objects.get(id = location)
+    images = Image.objects.filter(location = selected_location.id)
+    return render(request, 'location.html', {"location":selected_location,"images":images})
 
+def search(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+    return render(request,'search.html',{"images":searched_images,"category":search_term})
 
